c94118ed5c002d4353dca1a3b9346d25f9c89e7e
German Quiroz Bogner <ger@palmfdn.org>
2024-04-19 16:43:32 -0300

    carga y sumatoria de ingresos

--- Proyecto Js - Coder House/js/main.js
+++ Proyecto Js - Coder House/js/main.js
@@ -1,19 +1,77 @@
+const movementList							=	document.getElementById('movements')
+const userSelect            				=   document.getElementById('user-select')
+const movementSection       				=   document.getElementById('movement-section')
+const totalMovementsDisplay					=	document.getElementById('total-movements')
+
+let totalMovements							=	0
+let gastos									=	[]
+let userId									=	null
+
+function listMovement(form) {
+
+	totalMovements							=	0
+
+	gastos.push({
+		name	:	document.getElementById('movement-name').value,
+		value	:	document.getElementById('movement-amount').value
+	})
+
+	movementList.innerHTML					=	''
+
+	cargarGastos(userId)
+
+}
+
+function updateTotal() {
+
+	totalMovementsDisplay.textContent	=	totalMovements.toFixed(2)
+
+}
+
+async function cargarGastos(usuario) {
+		
+	try {
+
+		// Simulación de carga de gastos (aquí realizarías la solicitud al servidor)
+		console.log(`Cargando gastos para el usuario: ${usuario.name}`)
+
+		// Supongamos que obtienes los gastos del usuario desde una API
+		// const response					=	await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${usuario.id}`)
+		// gastos							=	await response.json()
+
+		console.log("Gastos", gastos)
+
+		// Obtener la lista de movimientos
+		
+		// Mostrar los gastos en la interfaz de usuario
+		gastos.forEach(gasto => {
+
+			const movementItem			=	document.createElement('li')
+			movementItem.textContent	=	`Gasto: ${gasto.name} - $${gasto.value}`
+			movementList.appendChild(movementItem)
+
+			// Sumar el gasto al total de movimientos
+			totalMovements				+=	parseFloat(gasto.value)
+
+			updateTotal()
+
+		})
+
+	} catch (error) {
+
+		console.error('Error al cargar los gastos:', error.message)
+
+	}
+
+}
+
 document.addEventListener('DOMContentLoaded', async function() {
 	
-	console.log("Init")
 	
-	const userSelect            			=   document.getElementById('user-select')
-	const movementSection       			=   document.getElementById('movement-section')
-	const totalMovementsDisplay				= document.getElementById('total-movements')
 
-	let totalMovements						=	0
 	let usuarios							=	[]
 
-	function updateTotal() {
-
-		totalMovementsDisplay.textContent	=	totalMovements.toFixed(2)
 	
-	}
 
 	async function obtenerUsuarios() {
 		
@@ -73,47 +131,9 @@ document.addEventListener('DOMContentLoaded', async function() {
 
 	}
 
-	async function cargarGastos(usuario) {
-		
-		try {
-
-			// Simulación de carga de gastos (aquí realizarías la solicitud al servidor)
-			console.log(`Cargando gastos para el usuario: ${usuario.name}`)
-	
-			// Supongamos que obtienes los gastos del usuario desde una API
-			const response					=	await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${usuario.id}`)
-			const gastos					=	await response.json()
-
-			console.log("Gastos", gastos)
-	
-			// Obtener la lista de movimientos
-			const movementList				=	document.getElementById('movements')
-	
-			// Mostrar los gastos en la interfaz de usuario
-			gastos.forEach(gasto => {
-
-				const movementItem			=	document.createElement('li')
-				movementItem.textContent	=	`Gasto: ${gasto.id}`
-				movementList.appendChild(movementItem)
-	
-				// Sumar el gasto al total de movimientos
-				totalMovements				+=	parseFloat(gasto.id)
-
-				updateTotal()
-
-			})
-
-		} catch (error) {
-
-			console.error('Error al cargar los gastos:', error.message)
-
-		}
-
-	}
-
 	userSelect.addEventListener('change', async function() {
 	
-		const userId						=	parseInt(userSelect.value)
+		userId								=	parseInt(userSelect.value)
 		
 		if(isNaN(userId)) return
 
